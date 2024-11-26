61c537dee4b7687992e647a996675bddd34901b9
RamzGxz <ranzdaffa32@gmail.com>
2024-11-26 14:26:52 +0700

    add shadcnUI & structured app

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm lg:px-3 px-6'>
+      this is navbar
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import AppShell from "@/components/appshell";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange>
+      <AppShell>
+        <Component {...pageProps} />
+      </AppShell>
+    </ThemeProvider>
+  );
 }
